a070613218a9f75f86cc0cd10a70b19ba52f7a8b
Dario Campagna <campagna@esteco.com>
2019-11-13 11:52:01 +0100

    Step 1 completed
    Signed-off-by: Dario Campagna <campagna@esteco.com>

--- test/source/test/AddNumbersTest.java
+++ test/source/test/AddNumbersTest.java
@@ -1,4 +1,25 @@
 package test;
 
+import dssc.calculator.StringCalculator;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class AddNumbersTest {
+
+    @Test
+    void emptyString() {
+        assertThat(StringCalculator.add(""), is(0));
+    }
+
+    @Test
+    void oneNumber() {
+        assertThat(StringCalculator.add("1"), is(1));
+    }
+
+    @Test
+    void twoNumbers() {
+        assertThat(StringCalculator.add("1,2"), is(3));
+    }
 }
